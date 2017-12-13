afa27b33d01781409f7da663f9caa5e720447f7a
miniPa <eric.fourpackcn@outlook.com>
2017-12-13 11:57:15 +0800

    scconsumer

--- eureka_producer_comsumer/spring_cloud_consumer/src/main/java/com/minipa/scd/scc/SpringCloudConsumerApplication.java
+++ eureka_producer_comsumer/spring_cloud_consumer/src/main/java/com/minipa/scd/scc/SpringCloudConsumerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-
 /**
  * SpringCloudConsumerApplication:
  *
