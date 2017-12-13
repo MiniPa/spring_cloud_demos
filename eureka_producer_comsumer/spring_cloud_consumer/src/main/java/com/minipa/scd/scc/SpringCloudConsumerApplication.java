496855c18047534336bae85776e60c5451219a73
miniPa <eric.fourpackcn@outlook.com>
2017-12-13 08:48:51 +0800

    Initial commit

---
+++ eureka_producer_comsumer/spring_cloud_consumer/src/main/java/com/minipa/scd/scc/SpringCloudConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.minipa.scd.scc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConsumerApplication.class, args);
+	}
+}
