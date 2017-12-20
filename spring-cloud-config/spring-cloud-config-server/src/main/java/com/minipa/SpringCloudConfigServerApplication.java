ee464cfe2f46f736a4a4b5a0a4710c509f334911
miniPa <eric.fourpackcn@outlook.com>
2017-12-20 12:12:09 +0800

    scconsumer

---
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/com/minipa/SpringCloudConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.minipa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+}
