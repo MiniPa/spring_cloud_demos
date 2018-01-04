213b192251a94a8512a371dfd70bac483d5902ad
miniPa <eric.fourpackcn@outlook.com>
2018-01-04 14:12:33 +0800

    cloud config eureka 高可用配置中心服务化改造

---
+++ spring-cloud-bus/spring-cloud-config-bus-client/terminal.cmd
@@ -0,0 +1,5 @@
+java -jar spring-cloud-bus\spring-cloud-config-bus-client\target\spring-cloud-config-bus-client-0.0.1-SNAPSHOT.jar --server.port=8021
+java -jar spring-cloud-bus\spring-cloud-config-bus-client\target\spring-cloud-config-bus-client-0.0.1-SNAPSHOT.jar --server.port=8022
+java -jar spring-cloud-bus\spring-cloud-config-bus-client\target\spring-cloud-config-bus-client-0.0.1-SNAPSHOT.jar --server.port=8023
+
+
